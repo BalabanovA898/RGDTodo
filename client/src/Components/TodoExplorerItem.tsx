d7b7e1c543d0c1b6759117caba84378cf8d7d7aa
Andrey Balabanov <balabaniada06@mail.ru>
2026-01-19 18:31:07 +0700

    add indicator whether list item is open

--- client/src/Components/TodoExplorerItem.tsx
+++ client/src/Components/TodoExplorerItem.tsx
@@ -12,16 +12,21 @@ export const TodoExplorerItem = (props: Props) => {
     const [active, setActive] = useState<boolean>(false);
     const [isTodoSelected, setTodoSelected] = useState<boolean>(props.isSelected);
     return <div className={["todo-explorer-item__container"].join(" ")}>
-        <h3 className="todo-explorer-item__titl"
-        onContextMenu={(e) => {
+        <div onContextMenu={(e) => {
             props.onClick(props.item);
             e.preventDefault();
         }} 
         onClick={(e) => {
             if (e.nativeEvent.button === 0)
                 setActive(!active);
-        }    
-        }>{props.item.value.title}</h3>
+        }}   className="todo-explorer__item__header-container" style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+            <h3 className="todo-explorer-item__title">{props.item.value.title}</h3>
+            <svg width="10px" height="10px" viewBox="0 0 20 20" style={{
+                rotate: active ? "180deg" : "0deg"
+            }}>
+                <path d="M 10 2 L 18 18 L 2 18" stroke="#fff" fill="#fff" strokeWidth={2}></path>
+            </svg>
+        </div>
         {
             active && props.item.children && 
                 <div className="todo-explorer-item__children">
